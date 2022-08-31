6388131be4083bcc076a87e00830b6346fcbba20
John <jsaita48@gmail.com>
2022-08-31 15:16:36 +0300

    configured user interactions

--- script.js
+++ script.js
@@ -1,129 +1,192 @@
-// Querry Character Name
-const charName =document.querySelector('h3');
-//Querry Character Image
-const charImg =document.querySelector('#image')
+document.addEventListener('DOMContentLoaded', () => {
+
+  //Character json-server URL
+  const url = 'https://api.disneyapi.dev/characters';
+  const URL = 'http://localhost:3000/characters';
+  // Querry Character Name
+  const charName = document.querySelector('h3');
+  //Querry Character Image
+  const charImg = document.querySelector('#image');
+
+  //querry like and Dislike Button button
+  const like = document.querySelector('#like');
+  const dislike = document.querySelector('#dislike');
 
 
 
-//random number
-const num =Math.floor(Math.random()*50);
-const num2=Math.floor(Math.random()*50);
+  //random number
+  const num = Math.floor(Math.random() * 50);
+  const num2 = Math.floor(Math.random() * 50);
 
 
-//GET data from Disney API
+//-------------------GET data from Disney API-------------------------------
 
-fetch('https://api.disneyapi.dev/characters')
-.then(res=>res.json())
-.then(data=>{
-  charName.innerText=data.data[num].name;
-  charImg.src=data.data[num].imageUrl;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      //-------------send Data to Local Server------------------------------
+      //Pesting Disney character to json server
+      let desneyData = data.data
+      fetch(URL)
+        .then(res => res.json())
+        .then(data => {
+          if (data.length === 0) {
+            //push data to json server
+            for (item of desneyData) {
+              fetchMethod(URL, 'POST', {
+                'name': item.name,
+                'imageUrl': item.imageUrl,
+                'likes': 0,
+                'dislike': 0,
+                'comments': []
+              })
+            }
+          }
+        });
+      //----------------------------------------------------------------------------
+    });
+  //--------------------------Fetching Data from Local api--------------------------
+  fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+      //-----------------search and comment----------------------------------------
+      //configuring search button
+      const searchBtn = document.querySelector('#search-form');
+
+      //querry user input
+      searchBtn.addEventListener('submit', (e) => {
+        const searchInput = document.querySelector('#search').value
+        e.preventDefault();
+        const array = data;
+        const searchObj = array.find(isTrue);
+
+        function isTrue(char) {
+          return char.name === searchInput;
+        }
+        if (typeof array.find(isTrue).name != 'undefined') {
+          charName.innerText = searchObj.name;
+          charImg.src = searchObj.imageUrl;
 
 
-//Configuring sidebr characters
-    //querry sidebar characters
-const sidebarChar=document.getElementsByClassName('preview');
+        } else {
+          charName.innerText = 'Character Does Not Exist'
+          charImg.src = "./images/dne.png"
+
+        }
+        searchBtn.reset()
+      });
+
+      //adding comments and user names
+      //Querry userName
+
+      const form = document.querySelector('#form');
+
+      form.addEventListener('submit', (e) => {
 
 
-for(val of sidebarChar){
-  const num2=Math.floor(Math.random()*50);
+        let user = form.querySelector('#userName').value;
+        const comment = form.querySelector('textarea').value;
 
-  const image=val.querySelector('img')
-  const char=val.querySelector('.char')
-  char.style.textAlign="center"
-  char.style.color='red'
-  image.src= data.data[num2].imageUrl;
-  char.innerText = data.data[num2].name;
+        e.preventDefault();
+        if (user === '' && comment != '') {
+          user = 'Anonymous-User';
+          submitEvent(user, comment);
+        } else if (user != '' && comment === '') {
+          alert('Add comment first')
+        } else if (user === '' && comment === '') {
+          alert('add comment first');
+        }
+        else {
+          submitEvent(user, comment)
+        }
+      });
 
-  //setting onclick listener to the sidebar div
-  val.addEventListener('click', ()=>{
-    charName.innerText = char.innerText;
-    charImg.src = image.src;
+      //Displaying comments on DOM
+
+      function submitEvent(user, comment) {
+
+        const d = new Date();
+        const year = d.getFullYear();
+        const month = d.getMonth();
+        const date = d.getDate();
+
+        let day = `${date}/${month}/${year}`
+
+        const li = document.createElement('li');
+        li.innerHTML = `<span>${user}: ${day} </span><p>${comment}</p>`
+        form.querySelector('ul').appendChild(li);
+        form.reset()
+      }
+//--------------------------------------------------------------------------------------
+
+ //---------------setting character displayed on the Page--------------------
+      charName.innerText = data[num].name;
+      charImg.src = data[num].imageUrl;
+
+       //Configuring sidebr characters
+            //querry sidebar characters
+            const sidebarChar = document.getElementsByClassName('preview');
+
+
+            for (val of sidebarChar) {
+              const num2 = Math.floor(Math.random() * 50);
+
+              const image = val.querySelector('img')
+              const char = val.querySelector('.char')
+              char.style.textAlign = "center"
+              char.style.color = 'red'
+              image.src = data[num2].imageUrl;
+              char.innerText = data[num2].name;
+              //setting onclick listener to the sidebar div
+              val.addEventListener('click', () => {
+                charName.innerText = char.innerText;
+                charImg.src = image.src;
+              });
+            }
+//-----------------------------------------------------------------------------------
+
+//-------------Setting Like and Dislike Button------------------------------------------
+
+              //like and dislike button configuration
+
+              //like button configuration
+
+              let count = 0
+
+              like.addEventListener('click', () => {
+                count++
+                like.innerHTML = `${count} &#128077;`;
+
+                //updating likes on the server
+                // fetchMethod(`${URL}/${charId}`, 'PATCH', { 'likes': count });
+              });
+              //dislike button configuration
+              let disCount = 0
+              dislike.addEventListener('click', () => {
+                disCount++
+                dislike.innerHTML = `${disCount} &#128078;`
+                //updating dislikes on the server
+                // fetchMethod(`${URL}/${charId}`, 'PATCH', { 'dislike': disCount });
+              });
+//--------------------------------------------------------------------------------------
+
+
+    });
+  //---------------------------------------------------------------------------------
+
+    });
+// Fetch Method POST AND PATCH
+function fetchMethod(url, method, obj) {
+  fetch(url, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(obj)
   })
+  .then(res => res.json())
+  .then(data => data)
+  .catch(error=>console.log(error))
 }
-
-  //configuring search button
-  const searchBtn = document.querySelector('#search-form');
-
-    //querry user input
-
-  searchBtn.addEventListener('submit', (e) => {
-    const searchInput=document.querySelector('#search').value
-    e.preventDefault();
-    const array = data.data;
-    const searchObj = array.find(isTrue);
-
-    function isTrue(char) {
-      return char.name === searchInput;
-    }
-    if (typeof array.find(isTrue) != 'undefined') {
-      charName.innerText = searchObj.name;
-      charImg.src = searchObj.imageUrl
-    } else {
-      charName.innerText = 'Character Does Not Exist Try Again'
-      charImg.src="./images/dne.png"
-    }
-    searchBtn.reset()
-  })
-})
-
-//like and dislike button configuration
-
-  //like button configuration
-
-const like=document.querySelector('#like');
-let count =0
-
-like.addEventListener('click',()=>{
- count++
- like.innerHTML=`${count} &#128077;`
-})
-
-  //dislike button configuration
-  let disCount=0;
-const dislike =document.querySelector('#dislike');
-dislike.addEventListener('click',()=>{
-  disCount++
-  dislike.innerHTML=`${disCount} &#128078; `
-
-})
-
-//adding comments and user names
-    //Querry userName
-
-    const form =document.querySelector('#form');
-
-form.addEventListener('submit',(e)=>{
-
-
-let user=form.querySelector('#userName').value;
-const comment=form.querySelector('textarea').value;
-
-  e.preventDefault();
-  if(user==='' && comment!=''){
-    user='Anonymous-User';
-    submitEvent(user,comment);
-  }else if(user !='' && comment ===''){
-    alert('Add comment first')
-  }else if(user==='' && comment===''){
-    alert('add comment first');
-  }
-  else{
-    submitEvent(user,comment)
-  }
-})
-function submitEvent(user,comment){
-
-  const d =new Date();
-  const year=d.getFullYear();
-  const month=d.getMonth();
-  const date =d.getDate();
-
-  let day=`${date}/${month}/${year}`
-
-  const li =document.createElement('li');
-  li.innerHTML=`<span>${user}: ${day} </span><p>${comment}</p>`
-  form.querySelector('ul').appendChild(li);
-  form.reset()
-}
-
 
